f4a76410170211a11c69f96cb3e8e5b84cf1175f
Mayank_01x <mayank.420.m@gmail.com>
2024-02-11 18:11:13 +0530

    added

---
+++ Calculator.py
@@ -0,0 +1,13 @@
+a = input("Enter First Number :")
+
+b = input("Enter Second Number :")
+
+print("The Sum of", (a) ,"+" ,(b) ,"is", int(a)+int(b))
+print("The Product of", (a) ,"*" ,(b) ,"is", int(a)*int(b))
+print("The Division of", (a) ,"÷" ,(b) ,"is", int(a)/int(b))
+print("The Division-Rounded Off to Nearest Integer of", (a) ,"÷" ,(b) ,"is", int(a)//int(b))
+print("The Subtraction of", (a) ,"-" ,(b) ,"is", int(a)-int(b))
+print("The Exponential of", (a) ,"^" ,(b) ,"is", int(a)**int(b))
+print("The Average of These Two Numbers is : " , (int(a) + int(b))/2)
+
+# Made By- \Mayank Aggarwal\
---
+++ Square Root Calculator.py
@@ -0,0 +1,6 @@
+a = int(input("Enter any Number : "))
+import math
+print("The Square Root is :" , math.sqrt(a))
+
+
+#Made By- \Mayank Aggarwal\
